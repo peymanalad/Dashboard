8ac55f941464a000723ac1f77b6348f048f84810
AmirDaryabak <amir.daryabak@gmail.com>
2025-07-28 13:55:58 +0330

    [FEATURE] WIP update cicd pipeline

--- deployments/scripts/docker-entrypoint.sh
+++ deployments/scripts/docker-entrypoint.sh
@@ -5,7 +5,6 @@ echo "Generating runtime environment variables..."
 
 cat <<EOF > /usr/share/nginx/html/env-config.js
 window.env = {
-  VITE_API_BASE_URL: "$VITE_API_BASE_URL",
   SKIP_PREFLIGHT_CHECK: "$DC_SKIP_PREFLIGHT_CHECK",
   REACT_APP_BASE_URL: "$DC_REACT_APP_BASE_URL",
   REACT_APP_STIMULSOFT_LICENCE_KEY: "$DC_REACT_APP_STIMULSOFT_LICENCE_KEY",
