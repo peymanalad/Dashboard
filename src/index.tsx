2107433e1077a5ab1ff449db006987e893418dc0
peyman <peyman.alad@gmail.com>
2025-08-31 13:24:59 +0330

    Fix Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
-import App from 'App';
+import {createRoot} from 'react-dom/client';import App from 'App';
 import {ServiceWorker} from 'libs';
 import 'App.css';
 import * as Sentry from '@sentry/react';
