a04a49d8153a034fcab421d813b658910e279534
peyman <peyman.alad@gmail.com>
2025-08-31 13:51:41 +0330

    Sentry package

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,8 @@ Sentry.init({
   dsn: 'https://2df0b1af7645b2a74c7bc2907ae6829b@sentry.stinascloud.ir/7',
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
-  sendDefaultPii: true
+  sendDefaultPii: true,
+  tracesSampleRate: 1.0,
 });
 
 const container = document.getElementById('root') as HTMLElement;
